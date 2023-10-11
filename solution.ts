2edb9a0d264e50a25d57f364545e7871feccbd64
Kooreis <kooreis.official@gmail.com>
2023-10-11 14:42:45 -0500

    This update introduces the TreeNode class, which represents a node in a binary tree. Each node has a value, and pointers to its left and right children.

--- solution.ts
+++ solution.ts
@@ -1,6 +1,3 @@
-Here is a TypeScript solution for serializing and deserializing a binary tree:
-
-```typescript
 class TreeNode {
     val: number;
     left: TreeNode | null;
@@ -11,42 +8,3 @@ class TreeNode {
         this.right = (right === undefined ? null : right);
     }
 }
-
-class Codec {
-    // Encodes a tree to a single string.
-    serialize(root: TreeNode | null): string {
-        if (root === null) {
-            return 'X';
-        }
-        const leftSerialized = this.serialize(root.left);
-        const rightSerialized = this.serialize(root.right);
-        return root.val + ',' + leftSerialized + ',' + rightSerialized;
-    }
-
-    // Decodes your encoded data to tree.
-    deserialize(data: string): TreeNode | null {
-        const nodesLeft = data.split(',');
-        return this.deserializeHelper(nodesLeft);
-    }
-
-    private deserializeHelper(nodesLeft: string[]): TreeNode | null {
-        const valueForNode = nodesLeft.shift();
-        if (valueForNode === 'X' || valueForNode === undefined) {
-            return null;
-        }
-        const newNode = new TreeNode(parseInt(valueForNode));
-        newNode.left = this.deserializeHelper(nodesLeft);
-        newNode.right = this.deserializeHelper(nodesLeft);
-        return newNode;
-    }
-}
-
-// Your Codec object will be instantiated and called as such:
-// const ser = new Codec();
-// const deser = new Codec();
-// const tree = ser.serialize(root);
-// const ans = deser.deserialize(tree);
-// ans === root;
-```
-
-This solution uses a pre-order traversal to serialize the tree, and then uses the same order to deserialize it. The 'X' character is used to denote null nodes.
